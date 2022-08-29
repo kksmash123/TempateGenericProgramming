4beaf3644385a97ca63800e803fec8fc71f43d95
kksmash123 <kksmashes@gmail.com>
2022-08-29 17:34:21 +0530

    template overloading

--- TempateGenericProgramming/TempateGenericProgramming.cpp
+++ TempateGenericProgramming/TempateGenericProgramming.cpp
@@ -7,7 +7,8 @@ template <typename T> void basicFun1(T x);
 template <typename T1, typename T2> void basicFun2(T1 x, T2 y);
 template <typename T1, typename T2 = int> void basicFun3(T1 x, T2 y);
 template <typename T> void BasicFun4(T x, int y);
-
+template <> void basicFun1<char>(char x);
+template <typename T> T findGreater(T x, T y);
 
 
 //template function definition
@@ -44,7 +45,15 @@ void BasicFun4(T x,int y)
 }
 
 
-//5.overloading template function called explicit specialization 
+//5. returning different datatype as per parameter , template used for returning
+template <typename T>
+T findGreater(T x, T y)
+{
+    return x >= y ? x : y;
+}
+
+
+//6.overloading template function called explicit specialization 
 //Explicitly overload function for specific datatype 
 //here overloading the basic single parameter template function for char datatype
 template <> void basicFun1<char>(char x)
@@ -53,11 +62,27 @@ template <> void basicFun1<char>(char x)
     cout << "value at x : " << x<<endl;
 }
 
-//6. returning different datatype as per parameter , template used for returning
+//7. Overloading the generic function template for number of parameter passed
+
 template <typename T>
-T findGreater(T x, T y)
+void templateoverload(T x)
 {
-    return x >= y ? x : y;
+    cout << "Template overload function with one parameter :" << endl;
+    cout << "value at x : " << x << endl;
+}
+
+template <typename T>
+void templateoverload(T x,T y)
+{
+    cout << "Template overload function with two parameter :" << endl;
+    cout << "value at x : " << x<<" | value at y : "<<y << endl;
+}
+
+template <typename T1,typename T2,typename T3>
+void templateoverload(T1 x, T2 y,T3 z)
+{
+    cout << "Template overload function with three different parameter :" << endl;
+    cout << "value at x : " << x << " | value at y : " << y <<" | Value at z : " <<z<< endl;
 }
 
 
@@ -83,6 +108,13 @@ int main()
     basicFun3("keerthana", "pavithra");
     basicFun3("keerthana", 'a');
 
+    //Different Return type using template function
+
+    cout << "Find Greater : " << endl;
+    cout << "Find Greater Results : " << findGreater(10, 10) << endl;
+    cout << "Find Greater Results : " << findGreater(5.5, 2.5) << endl;
+    
+
 
     //with constant parameter
 
@@ -94,12 +126,15 @@ int main()
     //it is much intelligent that convert float and char to its expected type 
     //but string can't convertable so it throws error
 
-   
-    //Different Return type using template function
+    //generic function overloading
+    cout << "Find Greater Results : " << findGreater('a', 'A') << endl;
 
-    cout << "Find Greater : " << endl;
-    cout << "Find Greater Results : " << findGreater(10, 10)<<endl;
-    cout << "Find Greater Results : " << findGreater(5.5, 2.5) << endl;
-    cout << "Find Greater Results : " << findGreater('a', 'A')<<endl;
 
+    cout << "Template Overloading with different number of parameters :" << endl;
+    //generic function template overloading
+
+    templateoverload(10);
+    templateoverload(10.5,20.5);
+    templateoverload(10, 'a', "keerthana");
+    
 }
